fdccf15ca792fd89a1ae67a5080055660bb0f648
Chris Micacchi <cmicacchi@solvco.com>
2013-05-06 16:51:20 -0400

    Support passive connections

--- lib/el_finder_ftp/railties.rb
+++ lib/el_finder_ftp/railties.rb
@@ -1,7 +1,6 @@
 module ElFinderFtp
   class Railties < ::Rails::Railtie
     initializer 'Rails logger' do
-      puts "In Railtie"
       ElFinderFtp::Connector.logger = Rails.logger
     end
   end
